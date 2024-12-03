294a87e0ba3adf0aebef204c5be3b2fc0ff9a1cd
Keshav09-Dev <keshav2234hisar@gmail.com>
2024-12-03 14:09:51 +0530

    updated

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use("/api/v1", require("./routes/userRoutes"));
 app.use("/api/v1", require("./routes/languageRoutes"));
 app.use("/api/v1", require("./routes/educationRoutes"));
 
-const PORT = 8000;
+const PORT = 10000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
